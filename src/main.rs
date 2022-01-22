b531b01968632e141a43d8e48b2088f7c41b7fb8
Daniel <daneager19@gmail.com>
2022-01-21 19:01:38 -0600

    Started data structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+enum Methods {
+    GET,
+    POST,
+}
+
+struct Request {
+    uri: String,
+    route: String,
+    port: u32,
+    method: Methods
+}
+
+impl Request {
+    fn method_type(&self.method) {
+        match {
+            self.method => String::from("GET") = {
+
+            },
+            self.method => String::from("POST") = {
+                
+            },
+        }
+    }
+}
+
 fn main() {
+    let req = Request {
+        uri: String::from("http://localhost"),
+        uri: String::from("/games"),
+        port: 5000,
+        method: String::from("GET"),
+    }
     println!("Hello, world!");
 }
